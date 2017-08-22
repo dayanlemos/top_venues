d248ce09932044ce3a86ccfc32b37675693335c2
Dayan Lemos <dayan-lemos@hotmail.com>
2017-08-23 00:01:18 +0100

    Creating service: VenueService (gets data from Foursquare API)

---
+++ src/services/venue.service.js
@@ -0,0 +1,20 @@
+angular.module('topVenuesModule').service('VenueService', ['$http', '$q', function($http, $q){
+
+    return{
+
+        getVenues: function () {
+
+            var api_method = '/venues/explore';
+            var api_default_params = 'near=Dublin,Ireland&limit=20&section=topPicks';
+
+            return $q(function (resolve, reject) {
+                $http.get( API_FOURSQUARE_URL + api_method+'?'+api_default_params+'&client_id='+API_FOURSQUARE_CLIENT_ID+'+&client_secret='+API_FOURSQUARE_CLIENT_SECRET+'&v=20171212').then(function (res){
+                    resolve(res.data.response);
+                }, function (err) {
+                    reject(err)
+                });
+            })
+        }
+    }
+
+}])
