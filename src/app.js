325756160d5c2e2f526783894906b9bdfba92515
Dayan Lemos <dayan-lemos@hotmail.com>
2017-08-22 23:01:54 +0100

    Creating views: venue-list and venue-details

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-var topVenuesModule = angular.module('topVenuesModule',[]);
+angular.module('topVenuesModule',['ui.router'])
+
+.config(['$stateProvider', '$locationProvider', function ($stateProvider, $locationProvider) {
+
+    $locationProvider.hashPrefix('');
+
+    $stateProvider.state({
+        name: 'venue-list',
+        url: '/venue-list',
+        templateUrl: 'views/venue-list.html'
+    })
+    .state({
+        name: 'venue-details',
+        url: '/venue-list/:id',
+        templateUrl: 'views/venue-details.html'
+    });
+}]);
