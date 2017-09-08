70c7268acc4b90c1566292901b65fee6b2b0a667
Dayan Lemos <dayan-lemos@hotmail.com>
2017-09-08 04:06:08 +0100

    Adding pagination

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ angular.module('topVenuesModule',['ui.router', 'ui.bootstrap'])
 
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    $urlRouterProvider.otherwise('venue-list');
+    $urlRouterProvider.otherwise('venue-list/1');
 
     $locationProvider.hashPrefix('');
 
     $stateProvider.state({
         cache: false,
         name: 'venue-list',
-        url: '/venue-list',
+        url: '/venue-list/:page',
         templateUrl: 'views/venue-list.html',
         controller: 'VenueListCtrl'
     })
     .state({
         name: 'venue-details',
-        url: '/venue-list/:id',
+        url: '/venue-list/:page/:id',
         templateUrl: 'views/venue-details.html',
         controller: 'VenueDetailsCtrl'
     });
--- src/controllers/venue-list.controller.js
+++ src/controllers/venue-list.controller.js
@@ -1,9 +1,21 @@
-angular.module('topVenuesModule').controller('VenueListCtrl', ['$scope','VenueService', function ($scope, VenueService) {
+angular.module('topVenuesModule').controller('VenueListCtrl', ['$scope', '$stateParams', '$state', '$log','VenueService', function ($scope, $stateParams, $state, $log, VenueService) {
 
+    $scope.location = 'Dublin';
     $scope.venues = {};
+    var page = $stateParams.page;
 
-    VenueService.getVenues().then(function (res) {
+    $scope.pageChanged = function(page) {
+        $state.go('venue-list', {page:page});
+    };
+
+    VenueService.getVenues(page).then(function (res) {
         $scope.venues = res;
+
+        //Pagination configs
+        $scope.currentPage = page;
+        $scope.totalItems = res.totalResults;
+        $scope.itemsPerPage = 15;
+
     }, function (err) {
         console.log(err);
     });
